78cfda6db1e8980b8cfe6b35bab2799080d940de
white-ninja <foreverquantum.leap@gmail.com>
2026-03-05 09:47:28 +0100

    Finished Prep 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        // Ask the user for their first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask the user for their last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Greet the user by their full name
+        Console.WriteLine();
+        Console.Write($"Hello, your name is {lastName}, {firstName} {lastName}! Welcome to C# programming.");
     }
 }
