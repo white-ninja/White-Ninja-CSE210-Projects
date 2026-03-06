530fb8610edc153593bd912d691bbaf29d6b3fc5
white-ninja <foreverquantum.leap@gmail.com>
2026-03-06 22:41:45 +0100

    C# Programming Exercise 4: Lists and Generics

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,81 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine();
+
+         string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+            int number = -1;
+
+            while (number != 0)
+            {
+                Console.Write("Enter number: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number != 0)
+                {
+                    numbers.Add(number);
+                }
+            }
+
+            int sum = 0;
+
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            double average = (double)sum / numbers.Count;
+
+            int max = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            int smallestPositive = int.MaxValue;
+
+            foreach (int num in numbers)
+            {
+                if (num > 0 && num < smallestPositive)
+                {
+                    smallestPositive = num;
+                }
+            }
+
+            numbers.Sort();
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+            Console.WriteLine("The sorted list is:");
+
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.Write("Do you want to run the program again? (yes/no): ");
+            playAgain = Console.ReadLine();
+
+            Console.WriteLine();
+        }
     }
 }
