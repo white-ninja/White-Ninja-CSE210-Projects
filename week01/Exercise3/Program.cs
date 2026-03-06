5e6da050a981c01538046c60efe17a51d60a8409
white-ninja <foreverquantum.leap@gmail.com>
2026-03-06 19:41:21 +0100

    Consolidated my knowledge of input, if statements, loops, and random numbers in C# by developing this randomNumberGuess Game.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+         string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guessCount = 0;
+
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            while (guess != magicNumber)
+            {
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+            }
+
+            guessCount++;
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
